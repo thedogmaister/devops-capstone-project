827d3c8def402804f1ecaa12ff0c016246463624
thedogmaister <kenyilpz@gmail.com>
2025-10-01 19:16:03 -0400

    add cors test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -189,3 +189,10 @@ class TestAccountService(TestCase):
         }
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
+
+    def test_cors_security(self):
+        """Debería devolver un encabezado CORS"""
+        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Verificar el encabezado CORS
+        self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
